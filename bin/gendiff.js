e13a7b2396b77f95843f6910d24eaffbb49cc254
rabbit <barateligeorge@yandex.ru>
2021-06-21 09:57:42 +0300

    add arguments and option -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 const { Command } = require('commander');
 
 const program = new Command();
-program.version('0.0.1');
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .description('Compares two configuration files and shows a difference.');
 program.parse(process.argv);
